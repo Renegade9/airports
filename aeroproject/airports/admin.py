091eb69ccef74bf40939852bcf7ae794289b8399
Chris Saulit <chris.saulit@gmail.com>
2017-01-07 22:29:22 -0800

    model fixes

--- aeroproject/airports/admin.py
+++ aeroproject/airports/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from model_definitions import Facility
+
+# Make our model available via Django admin interface
+admin.site.register(Facility)
--- aeroproject/airports/model_definitions/facility.py
+++ aeroproject/airports/model_definitions/facility.py
@@ -21,7 +21,7 @@ class Facility(models.Model):
 
     # local_id is the agency's airport code
     # e.g. LVK - Livermore
-    local_id = models.CharField(max_length=3, db_index=True)
+    local_id = models.CharField('Local Identifier', max_length=3, db_index=True)
 
     # Facility Type (e.g. AIRPORT, HELIPORT, SEAPORT, etc.)
     facility_type = models.CharField(max_length=12)
@@ -33,30 +33,35 @@ class Facility(models.Model):
     city = models.CharField(max_length=64)
 
     # Name of the facility
-    facility_name = models.CharField(max_length=64)
+    facility_name = models.CharField(max_length=64, null=True)
 
     # Elevation and elevation units of the facility (e.g. 400 FT)
-    elevation = models.IntegerField()
-    elevation_units = models.CharField(max_length=2)
+    elevation = models.IntegerField(null=True)
+    elevation_units = models.CharField(max_length=2, null=True)
 
     # Traffic Pattern Altitude of the facility (e.g. 400 FT) not as an altitude but
     # rather height above ground level (AGL)
-    tpa_agl = models.IntegerField('Traffic Pattern Altitude (TPA), distance above ground (AGL)')
-    tpa_agl_units = models.CharField(max_length=2)
+    tpa_agl = models.IntegerField('Traffic Pattern Altitude (TPA), distance above ground (AGL)',
+                                  null=True)
+    tpa_agl_units = models.CharField('TPA (AGL) Units', max_length=2, null=True)
 
     # Magnetic Variation, degrees and direction (E or W), and year
-    magnetic_variation_degrees = models.IntegerField()
-    magnetic_variation_direction = models.CharField(max_length=2)
-    magnetic_variation_year = models.IntegerField()
+    magnetic_variation_degrees = models.IntegerField(null=True)
+    magnetic_variation_direction = models.CharField(max_length=2, null=True)
+    magnetic_variation_year = models.IntegerField(null=True)
 
     # CTAF
     ctaf_frequency = models.DecimalField('Common Traffic Advisory Frequency (CTAF)',
                                          max_digits=6, decimal_places=3)
 
     # ICAO Airport Identifier (e.g. "KSFO", "CYEG")
-    icao_id = models.CharField(max_length=4, db_index=True)
+    icao_id = models.CharField('ICAO Identifier', max_length=4, db_index=True, null=True)
 
     # Is public use?  Null means unknown.
     is_public_use = models.NullBooleanField()
 
+    # Date the airport became active
+    activation_date = models.DateField(null=True)
 
+    def __str__(self):
+        return self.local_id + ' (' + self.city + ')'
