1bbaa3fa8c17ad44bf699700ab48c244760688ee
Chris Saulit <chris.saulit@gmail.com>
2017-12-16 15:48:58 -0800

    restructure directories -- flatten

--- aeroproject/aeroproject/urls.py
+++
@@ -1,9 +0,0 @@
-# aeroproject Top-level URL Configuration
-
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^airports/', include('airports.urls')),
-]
--- aeroproject/airports/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from airports.models import Facility
-
-# Make our model available via Django admin interface
-admin.site.register(Facility)
