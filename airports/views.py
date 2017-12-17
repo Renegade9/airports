91743a79fb681a35a20cdc427c218cedf8d7010e
Chris Saulit <chris.saulit@gmail.com>
2017-12-17 14:01:59 -0800

    make endpoints public, also fix boolean filter (ugh)

--- airports/views.py
+++ airports/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 from airports.models import Facility
 
 from rest_framework import filters, generics, views, viewsets
+
+from rest_framework.permissions import AllowAny
 from airports.serializers import UserSerializer, FacilitySerializer
 
 import django_filters
@@ -35,6 +37,7 @@ class FacilityViewSet(viewsets.ModelViewSet):
 
 class FindFacilityByCode(views.APIView):
     """Find a facility by code (FAA or ICAO airport code)"""
+    permission_classes = (AllowAny,)
 
     def get(self, request, code):
 
@@ -54,21 +57,38 @@ class FindFacilityByCode(views.APIView):
                                 'message': 'Could not find: {}'.format(code)})
 
 
+class CaseInsensitiveBooleanFilter(django_filters.Filter):
+    """Agreed, we shouldn't have to do this"""
+
+    def filter(self, qs, value):
+        if value is not None:
+            lc_value = value.lower()
+            if lc_value in ('true', 'yes', 'y', 't', '1'):
+                value = True
+            elif lc_value in ('false', 'no', 'n', 'f', '0'):
+                value = False
+            return qs.filter(**{self.name: value})
+        return qs
+
+
 class FacilitiesFilter(django_filters.rest_framework.FilterSet):
 
     # use partial case insensitive filtering for city and facility name filters
-    city = django_filters.CharFilter(name="city", lookup_expr='icontains')
-    facility_name = django_filters.CharFilter(name="facility_name", lookup_expr='icontains')
+    city = django_filters.CharFilter(name='city', lookup_expr='icontains')
+    facility_name = django_filters.CharFilter(name='facility_name', lookup_expr='icontains')
 
-    facility_name = django_filters.CharFilter(name="state_or_prov")
+    is_public_use = CaseInsensitiveBooleanFilter(name='is_public_use')
+    state_or_prov = django_filters.CharFilter(name='state_or_prov')
 
     class Meta:
         model = Facility
-        fields = ['city', 'facility_name', 'state_or_prov']
+        fields = ['city', 'facility_name', 'is_public_use', 'state_or_prov']
 
 
 class FacilitiesList(generics.ListAPIView):
     """List Facilities with various filters"""
+    permission_classes = (AllowAny,)
+
     filter_backends = (filters.OrderingFilter, django_filters.rest_framework.DjangoFilterBackend)
     ordering_fields = ('local_id', 'icao_id', 'state_or_prov')
     filter_class = FacilitiesFilter
